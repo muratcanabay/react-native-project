53b4f4a910bcbc827b38c32a2f7b85739864cd26
muratcanabay <muratcanabay@hotmail.com>
2021-02-12 14:40:56 +0300

    fonts added & social media buttons added & sigupscreen added.

--- App.js
+++ App.js
@@ -1,44 +1,8 @@
-import React, { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import OnboardingScreen from './screens/OnboardingScreen';
-import LoginScreen from './screens/LoginScreen'
-
-const AppStack = createStackNavigator();
+import React from 'react';
+import Providers from './navigation';
 
 const App = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
-
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLaunched').then(value => {
-      if (value == null) {
-        AsyncStorage.setItem('alreadyLaunched', 'true');
-        setIsFirstLaunch(true);
-      } else {
-        setIsFirstLaunch(false);
-      }
-    });
-  }, []);
-
-  if (isFirstLaunch === null) {
-    return null;
-  } else if (isFirstLaunch === true) {
-    return (
-      <NavigationContainer>
-        <AppStack.Navigator
-          headerMode='none'
-        >
-          <AppStack.Screen name='Onboarding' component={OnboardingScreen} />
-          <AppStack.Screen name='Login' component={LoginScreen} />
-        </AppStack.Navigator>
-      </NavigationContainer>
-    );
-  } else {
-    return <LoginScreen />;
-  }
+  return <Providers />;
 };
 
 export default App;
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,65 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import SocialButton from '../components/SocialButton';
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
     return (
         <View style={styles.container}>
-            <Text>Login Screen</Text>
-            <Button
-                title="Click Here"
-                onPress={() => alert('Button Clicked!')}
+            <Image
+                source={require('../assets/rn-social-logo.png')}
+                style={styles.logo}
             />
+            <Text style={styles.text}>Video to 3D Model</Text>
+
+            <FormInput
+                labelValue={email}
+                onChangeText={(userEmail) => setEmail(userEmail)}
+                placeholderText="Email"
+                iconType="user"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <FormInput
+                labelValue={password}
+                onChangeText={(userPassword) => setPassword(userPassword)}
+                placeholderText="Password"
+                iconType="lock"
+                secureTextEntry={true}
+            />
+            <FormButton
+                buttonTitle="Sign In"
+                onPress={() => alert('Sign In Clicked!')}
+            />
+            <TouchableOpacity style={styles.forgotButton} onPress={() => { }}>
+                <Text style={styles.navButtonText}>Forgot Password?</Text>
+            </TouchableOpacity>
+            <SocialButton
+                buttonTitle="Sign In with Facebook"
+                btnType="facebook"
+                color="#4867aa"
+                backgroundColor="#e6eaf4"
+                onPress={() => { }}
+            />
+
+            <SocialButton
+                buttonTitle="Sign In with Google"
+                btnType="google"
+                color="#de4d41"
+                backgroundColor="#f5e7ea"
+                onPress={() => { }}
+            />
+            <TouchableOpacity
+                style={styles.forgotButton}
+                onPress={() => navigation.navigate('Signup')}>
+                <Text style={styles.navButtonText}>
+                    Don't have an acount? Create here
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -17,8 +68,34 @@ export default LoginScreen;
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#f9fafd',
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20,
+        paddingTop: 50,
+    },
+    logo: {
+        height: 150,
+        width: 150,
+        resizeMode: 'cover',
+    },
+    text: {
+        fontFamily: 'Kufam-SemiBoldItalic',
+        fontSize: 28,
+        marginBottom: 10,
+        color: '#051d5f',
+    },
+    navButton: {
+        marginTop: 15,
+    },
+    forgotButton: {
+        marginVertical: 35,
+    },
+    navButtonText: {
+        fontSize: 18,
+        fontWeight: '500',
+        color: '#2e64e5',
+        fontFamily: 'Lato-Regular',
     },
 });
