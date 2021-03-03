f2c32e7e709f28ed8398c792cfde2820759a90f2
muratcanabay <muratcanabay@hotmail.com>
2021-03-03 03:56:50 +0300

    Lottie animations added to Onboarding Screen.

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
+import LottieView from 'lottie-react-native';
 
 const Skip = ({...props}) => (
   <TouchableOpacity style={{marginHorizontal: 10}} {...props}>
@@ -50,34 +51,80 @@ const OnboardingScreen = ({navigation}) => {
       }}
       pages={[
         {
-          backgroundColor: '#81ecec',
+          /* 1. Adım */
+          backgroundColor: '#BFDCCF',
           image: (
-            <Image
-              source={require('../assets/onboarding/onboarding-img1.png')}
-            />
+            <View style={styles.container}>
+              <LottieView
+                style={{width: '100%'}}
+                source={require('../assets/lottie/360-phone.json')}
+                autoPlay
+                loop
+                width={250}
+                height={250}
+              />
+            </View>
           ),
-          title: 'Onboarding 1',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          title: (
+            <Text style={styles.title}>
+              Yüzünüzü video kaydı boyunca 360<Text>{'\u00b0'}</Text> açı ile
+              çekin
+            </Text>
+          ),
+          subtitle: (
+            <Text style={styles.subtitle}>
+              Farklı açılardan video kaydı model kalitenizi arttıracaktır
+            </Text>
+          ),
         },
+        /* 2. Adım */
         {
-          backgroundColor: '#fab1a0',
+          backgroundColor: '#F48FB1',
           image: (
-            <Image
-              source={require('../assets/onboarding/onboarding-img2.png')}
-            />
+            <View style={styles.container}>
+              <LottieView
+                style={{width: '100%'}}
+                source={require('../assets/lottie/upload.json')}
+                autoPlay
+                loop
+                width={250}
+                height={250}
+              />
+            </View>
           ),
-          title: 'Onboarding 2',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          title: <Text style={styles.title}>Videonuzu bizimle paylaşın</Text>,
+          subtitle: (
+            <Text style={styles.subtitle}>
+              Videolarınız sadece yetkili kişiler tarafından erişilebilen{' '}
+              <Text style={{color: '#1897FF'}}>Google Cloud</Text> Veritabanında
+              saklanmaktadır
+            </Text>
+          ),
         },
+        /* 3. Adım */
         {
-          backgroundColor: '#a29bfe',
+          backgroundColor: '#FFF59D',
           image: (
-            <Image
-              source={require('../assets/onboarding/onboarding-img3.png')}
-            />
+            <View style={styles.container}>
+              <LottieView
+                style={{width: '100%'}}
+                source={require('../assets/lottie/cargo.json')}
+                autoPlay
+                loop
+                width={250}
+                height={250}
+              />
+            </View>
           ),
-          title: 'Onboarding 3',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          title: (
+            <Text style={styles.title}>Siparişiniz hemen teslim edilsin</Text>
+          ),
+          subtitle: (
+            <Text style={styles.subtitle}>
+              Siparişiniz ödeme yaptıktan sonra 1 hafta içinde teslim
+              edilecektir
+            </Text>
+          ),
         },
       ]}
     />
@@ -85,3 +132,23 @@ const OnboardingScreen = ({navigation}) => {
 };
 
 export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 40,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    textAlign: 'center',
+    paddingBottom: 70,
+  },
+  subtitle: {
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#32384D',
+  },
+});
